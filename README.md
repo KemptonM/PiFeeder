56262d668d88399e2a47a731f9061e16aff2f05c
KemptonM <kempton.maillett@gmail.com>
2021-07-11 17:54:56 -0400

    Put files back together

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ This project uses a Raspberry Pi Zero with buttons and LEDs to track my dog's fe
 
 ## Planned features:
 - Sort and upload data to a web API
-- Send notifications on mobile when feed time is missed
-- Visualize data on a website
+- Send notifications on mobile when light has been left on for 1 hour
+- Visualize feeding time data on a website
--- main.py
+++ main.py
@@ -1,25 +1,33 @@
+import schedule
+import time
 from gpiozero import LED, Button    # might not work on the Pi
 
 wButton = Button(0) # update button number
 wLED = LED(0)       # update LED number
-yButton = Button(0)
-yLED = LED(0)
+yButton = Button(0) # update button number
+yLED = LED(0)       # update LED number
 
 wFed = False
 yFed = False
 
-if wButton.when_pressed():
+def willieFed():
     wLED.off
-    wFed = True
-if yButton.when_pressed():
+    print("Wille button pressed")
+def yodaFed():
     yLED.off
-    yFed = True
-print("Recording...")       # how will I record this?
+    print("Yoda button pressed")
 
 # Every day at 07:00 and 18:00 timeToFeed() is called
 def timeToFeed():
-    wFed = False
     wLED.on         # turn on Willie's LED
-    yFed = False
     yLED.on         # turn on Yoda's LED
-    print("Lights have turned on")
+    print("Both lights have turned on")
+
+schedule.every().day.at("07:00").do(timeToFeed)
+schedule.every().day.at("18:00").do(timeToFeed)
+wButton.when_pressed = willieFed
+yButton.when_pressed = yodaFed
+
+while True:
+    schedule.run_pending()  # checks for pending tasks
+    time.sleep(1)           # loops every second
